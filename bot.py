5df9537ea0e2564d06ede0410227e7815af4b6ac
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-21 18:42:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,23 +47,22 @@ def view_pokedex(client, message):
 
 # Handler function for /catch command
 @app.on_message(filters.command("catch"))
-def catch_pokemon(client, message):
-    user_input = message.text
-    pokemon_name = user_input.split("/catch ")[-1].lower()
-
+def catch_pokemon(update, context):
+    user_input = update.message.text
+    pokemon_name = user_input.split("/catch ", 1)[-1].lower()
     if announced_pokemon is None:
-        client.send_message(message.chat.id, "No Pokémon is currently announced.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="No Pokémon is currently announced.")
         return
-
     if pokemon_name == announced_pokemon["name"].lower():
         catch_probability = random.random()
         if catch_probability <= announced_pokemon["catch_rate"]:
-            client.send_message(message.chat.id, "Congratulations! You caught {}!".format(announced_pokemon["name"]))
-            add_to_pokedex(message.from_user.id, announced_pokemon["name"])
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Congratulations! You caught {}!".format(announced_pokemon["name"]))
+            add_to_pokedex(update.message.from_user.id, announced_pokemon["name"])
         else:
-            client.send_message(message.chat.id, "Oh no! {} escaped!".format(announced_pokemon["name"]))
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Oh no! {} escaped!".format(announced_pokemon["name"]))
     else:
-        client.send_message(message.chat.id, "The announced Pokémon is not {}.".format(pokemon_name))
+        context.bot.send_message(chat_id=update.effective_chat.id, text="The announced Pokémon is not {}.".format(pokemon_name))
+
 
 # Handler function for group messages
 @app.on_message(filters.group)
