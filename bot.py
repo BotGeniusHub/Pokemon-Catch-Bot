f62ded3ab0eebc507c6f50c5cfd42fe6df0eaec8
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-21 18:38:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import random
 import requests
 from pymongo import MongoClient
@@ -71,7 +72,7 @@ def group_message(client, message):
 
     message_count += 1
 
-    if message_count % 100 == 0:
+    if message_count % 10 == 0:
         announced_pokemon = random.choice(pokemon_database)
         pokemon_data = pokemon(announced_pokemon["name"].lower())
         pokemon_image_url = pokemon_data.sprites.front_default
