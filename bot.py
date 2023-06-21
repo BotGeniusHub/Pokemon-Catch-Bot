76bfd9582baef9157331cf1cc086df56813087e8
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-22 00:25:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -718,15 +718,18 @@ def start(_, message):
     )
 
 @app.on_message(filters.command("help"))
-def help_command(_, message):
-    help_text = "Welcome to the Pokémon Catching Bot!\n\n" \
-                "Commands:\n" \
-                "/start - Start the bot and encounter a wild Pokémon\n" \
-                "/catch - Attempt to catch the encountered Pokémon\n" \
-                "/help - Display this help menu\n" \
-                "/pokedex - To view your pokémon"
-
-    app.send_message(chat_id=message.chat.id, text=help_text)
+def help_command(client, message):
+    
+    image_path = "https://telegra.ph/file/c858bf6f6e617b59c986a.jpg"  
+    with open(image_path, "rb") as image_file:
+        caption = "Welcome to the Pokémon Catching Bot!\n\n" \
+                "Commands:\n" \
+                "/start - Start the bot and encounter a wild Pokémon\n" \
+                "/catch - Attempt to catch the encountered Pokémon\n" \
+                "/help - Display this help menu\n" \
+                "/pokedex - View your Pokémon"
+                       
+        client.send_photo(chat_id=message.chat.id, photo=image_file, caption=caption, reply_to_message_id=message.message_id)
 
                   
 # Handler function for /pokedex command
