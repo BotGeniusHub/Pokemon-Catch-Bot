a68f694402754211d7c36a15b2a960550fcabe6a
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-26 18:30:50 +0530

    Create pokestore.py

---
+++ pokestore.py
@@ -0,0 +1,20 @@
+# Replace with your own logic to load Pokémon store data
+    # Example: Fetch data from a database or API
+    # Here, we are using a static list for demonstration purposes
+
+    # Pokemon store data structure:
+    # [
+    #   {"name": "pokemon1", "image": "image_url1", "price": 50},
+    #   {"name": "pokemon2", "image": "image_url2", "price": 75},
+    #   ...
+    # ]
+
+    pokemon_store = [
+        {"name": "charmander", "image": "charmander_image_url", "price": 50},
+        {"name": "bulbasaur", "image": "bulbasaur_image_url", "price": 60},
+        {"name": "squirtle", "image": "squirtle_image_url", "price": 70},
+        {"name": "pikachu", "image": "pikachu_image_url", "price": 80},
+        {"name": "jigglypuff", "image": "jigglypuff_image_url", "price": 90},
+        {"name": "eevee", "image": "eevee_image_url", "price": 100},
+        # Add more Pokémon entries for additional pages
+    ]
